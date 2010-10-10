0f729a95415f1fb3af5e81e97180d5e2b96ccda4
Johannes Fasching <fasching@golemdigital.at>
2010-10-10 22:49:18 +0200

    - Notebook backup

--- server/lib/httpmethods/stationmethods.js
+++ server/lib/httpmethods/stationmethods.js
@@ -70,23 +70,76 @@ HTTP.methods({
             console.log("data", postData)
             if(!postData.stationId)
                 return "Error: no stationId provided"
-            var result = db.Competition23.find({
-                    stationId:postData.stationId
-                },
-                {
-                    fields: {
-                        pin: 1,
-                        gamePoints: 1,
-                        usedTime:1,
-                        _id:0
+
+
+            var result = db.Competition23.aggregate([
+
+                    {   $match:
+                        {
+                            stationId:postData.stationId
+                        }
                     },
-                    sort: {
-                        gamePoints: -1
+                    { "$sort":
+                        {
+                            "totalPoints": -1
+                        }
                     },
-                    limit: 50
+                    {    $group:
+                        {
+                            _id: "$pin",
+                            totalPoints: { $max: "$totalPoints" },
+                            pin : { $first: '$pin' },
+                            time : { $first: '$time' }
+                        }
+                    },
+                    { "$sort":
+                        {
+                            "totalPoints": -1
+                        }
+                    }
+
+                ]
+            )
+
+            console.log("XXXXX", JSON.stringify(result) )
+
+            //var result = db.Competition23.find({
+            //        stationId:postData.stationId
+            //    },
+            //    {
+            //        field
+            // 0s: {
+            //            pin: 1,
+            //            totalPoints: 1,
+            //            time:1,
+            //            _id:0
+            //        },
+            //        sort: {
+            //            gamePoints: -1
+            //        },
+            //        limit: 50
+            //    }
+            //).fetch();
+
+            var res = []
+
+            _.forEach(result, function(item){
+                var userPin = db.Pin.findOne({pin:item.pin});
+                var userName = "anonymous";
+                if(userPin && userPin.userId) {
+                    var mUser = Meteor.users.findOne(
+                        {_id:userPin.userId}
+                    )
+                    if(mUser && mUser.profile && mUser.profile.userName) {
+                        userName = mUser.profile.userName;
+                    }
                 }
-            ).fetch();
-            return ("{\"data\":" + JSON.stringify(result) + "}" )
+
+                var entry = { totalPoints:item.totalPoints, time:item.time, userName:userName, pin:item.pin }
+                res.push( entry )
+            });
+
+            return ("{\"data\":" + JSON.stringify(res) + "}" )
         }
     },
     '/api/user': {
@@ -95,26 +148,177 @@ HTTP.methods({
             console.log("data", postData)
             if(!postData.pin || !postData.stationId)
                 return "Error: no pin provided"
-            var result = db.Competition23.find({
-                    pin:postData.pin,
-                    stationId: postData.stationId
-                },
-                {
-                    fields: {
-                        gamePoints: 1,
-                        usedTime:1,
-                        stationId:1,
-                        _id:0
+
+            var result = db.Competition23.aggregate([
+
+                    {   $match:
+                        {
+                            stationId:postData.stationId
+                        }
                     },
-                    sort: {
-                        gamePoints: -1
+                    { "$sort":
+                        {
+                            "totalPoints": -1
+                        }
                     },
-                    limit: 50
+                    {    $group:
+                        {
+                            _id: "$pin",
+                            totalPoints: { $max: "$totalPoints" },
+                            pin : { $first: '$pin' },
+                            time : { $first: '$time' }
+                        }
+                    },
+                    { "$sort":
+                        {
+                            "totalPoints": -1
+                        }
+                    }
+
+                ]
+            )
+
+            console.log("XXXXX1", JSON.stringify(result) )
+
+
+            var rank = 1
+            var data;
+
+            _.forEach(result, function(item){
+                console.log(1)
+                if(item.pin == postData.pin) {
+                    console.log(2)
+                    var userPin = db.Pin.findOne({pin: item.pin});
+                    var userName = "anonymous";
+                    if (userPin && userPin.userId) {
+                        console.log(3)
+                        var mUser = Meteor.users.findOne(
+                            {_id: userPin.userId}
+                        )
+                        if (mUser && mUser.profile && mUser.profile.userName) {
+                            userName = mUser.profile.userName;
+                        }
+                    }
+
+                    var entry = {
+                        totalPoints: item.totalPoints,
+                        time: item.time,
+                        userName: userName,
+                        pin: item.pin,
+                        rank: rank,
+                        stationId: postData.stationId
+                    }
+
+                    console.log("XXXXX2", JSON.stringify(entry) )
+
+                    data = entry;
+                    return;
+
+                    //return ("{\"data\":" + JSON.stringify(entry) + "}" )
                 }
-            ).fetch();
-            return ("{\"data\":" + JSON.stringify(result) + "}" )
+                else
+                    rank++;
+            });
+
+            //var result = db.Competition23.findOne({
+            //        pin:postData.pin,
+            //        stationId: postData.stationId
+            //    },
+            //    {
+            //        fields: {
+            //            totalPoints: 1,
+            //            time:1,
+            //            stationId:1,
+            //            _id:0
+            //        },
+            //        sort: {
+            //            totalPoints: -1
+            //        },
+            //        limit: 50
+            //    }
+            //);
+            return ("{\"data\":" + JSON.stringify(data) + "}" )
+        }
+    },
+    '/api/ranking': {
+        post: function (data) {
+            var postData = JSON.parse(data);
+            console.log("data", postData)
+            if(!postData.stationId)
+                return "Error: no stationId provided"
+
+            var lastNoName = db.Competition23.findOne(
+                {pin:"-1"}, {sort: {createdAt: -1}}
+
+            )
+
+            if(!lastNoName) {
+                return ("{\"data\":false}" )
+            }
+
+            var result = db.Competition23.aggregate([
+
+                    {   $match:
+                    {
+                        stationId:postData.stationId
+                    }
+                    },
+                    { "$sort":
+                    {
+                        "totalPoints": -1
+                    }
+                    },
+                    {    $group:
+                    {
+                        _id: "$pin",
+                        totalPoints: { $max: "$totalPoints" },
+                        pin : { $first: '$pin' },
+                        time : { $first: '$time' }
+                    }
+                    },
+                    { "$sort":
+                    {
+                        "totalPoints": -1
+                    }
+                    }
+
+                ]
+            )
+
+            if(!result) {
+                return ("{\"data\":\"ranking\": 1}" )
+            }
+
+            console.log("XXXXX1", JSON.stringify(result) )
+
+
+            var rank = 1
+            var data;
+
+            _.forEach(result, function(item){
+                console.log(1)
+                if( lastNoName.totalPoints >= item.totalPoints ) {
+                    console.log(2)
+
+
+                    var entry = {
+                        totalPoints: lastNoName.totalPoints,
+                        time: lastNoName.time,
+                        ranking: rank,
+                    }
+
+                    console.log("XXXXX2", JSON.stringify(entry) )
+
+                    data = entry;
+                    return;
+
+                    //return ("{\"data\":" + JSON.stringify(entry) + "}" )
+                }
+                else
+                    rank++;
+            });
+
+            return ("{\"data\":" + JSON.stringify(data) + "}" )
         }
     }
-
-
 });
