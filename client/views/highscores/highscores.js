23fda75c02badd13c212df0d6d0507dd2e76efd2
Johannes Fasching <fasching@golemdigital.at>
2016-09-27 22:47:59 +0200

    - before recover SUI Meteor

---
+++ client/views/highscores/highscores.js
@@ -0,0 +1,18 @@
+/**
+ * Created by hannes on 27.09.16.
+ */
+
+
+
+Template.body.onRendered (function () {
+    function includeJs(jsFilePath) {
+        var js = document.createElement("script");
+
+        js.type = "text/javascript";
+        js.src = jsFilePath;
+
+        document.body.appendChild(js);
+    }
+    $.getScript( "app.js" )
+    //includeJs("app.js");
+})
